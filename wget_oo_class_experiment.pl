1141004fcba6818e0f9dbc70cd3172e374b8d9fc
triogh2 <theroadisopen@gmail.com>
2017-02-19 15:33:10 +0100

    adds conditional branching to show method

--- wget_oo_class_experiment.pl
+++ wget_oo_class_experiment.pl
@@ -36,8 +36,9 @@ sub new {
 
     my $self = {
         cmd => 'wget',
-        head => '--spider',
-        download => '-O',
+        op_head => '--spider',
+        download => $arg->{download} ? $arg->{download} : undef,
+        op_download => '-O',
         devnull => '/dev/null',
         headers => '-S',
         url => $arg->{url} ? $arg->{url} : 'https://en.wikipedia.org/wiki/Object-oriented_programming',
@@ -49,13 +50,27 @@ sub new {
 
 sub show {
     my $self = shift;
-    print $self->{cmd}, ' ' , $self->{head}, ' ', $self->{headers}, ' ', $self->{url}, "\n";
+
+    my @cmd;
+    if ($self->{download}) {
+        push @cmd, $self->{cmd}, $self->{op_download}, $self->{devnull}, $self->{headers}, $self->{url};
+    }
+    else {
+        push @cmd, $self->{cmd}, $self->{op_head}, $self->{headers}, $self->{url};
+    }
+    print "@cmd\n";
 }
 
 sub cmd_aref {
     my $self = shift;
-    #push my @cmd, $self->{cmd}, $self->{head}, $self->{headers}, $self->{url};
-    push my @cmd, $self->{cmd}, $self->{download}, $self->{devnull}, $self->{headers}, $self->{url};
+
+    my @cmd;
+    if ($self->{download}) {
+        push @cmd, $self->{cmd}, $self->{op_download}, $self->{devnull}, $self->{headers}, $self->{url};
+    }
+    else {
+        push @cmd, $self->{cmd}, $self->{op_head}, $self->{headers}, $self->{url};
+    }
     return \@cmd;
 }
 
@@ -68,8 +83,14 @@ package main;
 
 
 my $wget_cmd = WgetCmd->new( {url => 'http://lxer.com'} );
+my $wget_cmd_download = WgetCmd->new( { url => 'http://lxer.com', download => 1 } );
 $wget_cmd->show();
+$wget_cmd_download->show();
 my $cmd_aref = $wget_cmd->cmd_aref();
+my $cmd_aref_download = $wget_cmd_download->cmd_aref();
 print "@$cmd_aref", "\n";
+print "@$cmd_aref_download", "\n";
 print Dumper $cmd_aref;
+print Dumper $cmd_aref_download;
 $wget_cmd->execute();
+$wget_cmd_download->execute();
