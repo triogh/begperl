ea3ff91a1d5971fbee71909b9b210788bac59719
tomog999 <tomog999@ubuntuhp>
2017-03-13 15:59:16 +0100

    adds graphviz_test.pl

---
+++ graphviz_test.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+#===============================================================================
+#
+#         FILE: graphviz_test.pl
+#
+#        USAGE: ./graphviz_test.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 03/13/2017 03:50:43 PM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+use GraphViz;
+
+my $g = GraphViz->new();
+$g->add_node('Client');
+$g->add_node('Server', shape => 'box');
+$g->add_edge('Client' => 'Server');
+$g->as_jpeg('simple.jpg');
